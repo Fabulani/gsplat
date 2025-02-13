1428bc1c67d09dd96e494398011e306e422aa8d1
Fabiano Junior Maia Manschein <16107182+Fabulani@users.noreply.github.com>
2025-02-13 13:02:41 +0200

    Add check for cuda arch list

--- test_cuda.py
+++ test_cuda.py
@@ -6,3 +6,4 @@ assert torch.cuda.is_available(), "CUDA not available"
 print(f"-> GPU: {torch.cuda.get_device_name(0)}.")
 print(f"-> PyTorch version: {torch.__version__}.")
 print(f"-> PyTorch CUDA version: {torch.version.cuda}.")
+print(f"-> PyTorch CUDA arch list: {torch.cuda.get_arch_list()}.")
